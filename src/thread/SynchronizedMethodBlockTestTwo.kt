10160b56f2586df39d05cc28cdbfa07894e9ee28
liguoying <15510224541@163com>
2019-08-15 17:27:49 +0800

    add

---
+++ src/thread/SynchronizedMethodBlockTestTwo.kt
@@ -0,0 +1,57 @@
+package thread
+
+
+object MainBlockTestA {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        one()
+    }
+
+    private fun one() {
+        val task = TaskBlockTestTwo()
+        val threadAA = ThreadAAA(task)
+        threadAA.start()
+        val threadBB = ThreadBBB(task)
+        threadBB.start()
+    }
+}
+
+
+class TaskBlockTestTwo : Task {
+
+    override fun doLongTimeTask() {
+
+        synchronized(this) {
+            println("A begin time is :${System.currentTimeMillis()}" )
+            Thread.sleep(2000)
+            println("A end time is :${System.currentTimeMillis()}")
+        }
+    }
+
+    fun methodA(){
+        synchronized(this) {
+            println("B begin time is :${System.currentTimeMillis()}" )
+            Thread.sleep(2000)
+            println("B end time is :${System.currentTimeMillis()}")
+        }
+    }
+}
+
+
+class ThreadAAA(private val taskOne: Task) : Thread() {
+
+
+    override fun run() {
+        super.run()
+        taskOne.doLongTimeTask()
+    }
+}
+
+class ThreadBBB(private val taskOne: TaskBlockTestTwo) : Thread() {
+
+
+    override fun run() {
+        super.run()
+        taskOne.methodA()
+    }
+}
